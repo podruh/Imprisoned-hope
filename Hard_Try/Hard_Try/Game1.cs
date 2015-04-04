d46573824bf39a9c0756bcc5c0b1da5bb77950ab
w00d3nl3g <filip5871@gmail.com>
2015-04-04 19:03:33 +0200

    0.0026 - Nový konstruktor třídy Menu + popisky class
    + nový konstruktor třídy Menu pro NewGame menu s odsazením a mezerami
    řešen po ose X
    + novinové popisky ke classam
    BUG - textura NG menu má špatné parametry
    +nový font Times
    fix - prozatím se neloaduje hudba

--- Hard_Try/Hard_Try/Game1.cs
+++ Hard_Try/Hard_Try/Game1.cs
@@ -19,8 +19,12 @@ namespace Imprisoned_Hope
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        #region stringy popisu class
+        private string mastermindNews = "Druh mutace ovlivòující mozkovou èinnost se poprvé objevil v období 30. let" + Environment.NewLine + " 21. století, pojmenovaném Nová Generace, jako modifikace ji veøejnì známého" + Environment.NewLine + " mutagenu, jeho obìti jsou nazıvány jako The Others. Jedinci ovlivnìni touto" + Environment.NewLine + " mutací byli nazıváni kódovım oznaèením Mastermind vzhledem k jejich" + Environment.NewLine + " schopnostem manipulovat s lidskou myslí. První pøípad vıskytu této mutace byl" + Environment.NewLine + "nalezen u eny jménem Eliza Green. Tato ena sídlící v New Hampshire se ji" + Environment.NewLine + " od útlého vìku ivila v cirkuse jako vìdma a byla tam tøikrát na hranici smrti" + Environment.NewLine + " z dùvodu útoku z publika." + Environment.NewLine + "Po objevení této mutace byla pøedána do rukou vìdcù, kteøí nyní zkoumají její" + Environment.NewLine + " mutaci. Tímto chci uklidnit všechny ètenáøe, aby nepanikaøili z dùvodu domnìní," + Environment.NewLine + " e tato mutace je nakalivá. Jedná se pouze o pøetvoøení DNA majitele a tudí nelze" + Environment.NewLine + " pøenášet ani vzduchem ani krví.";
+        private string enforcerNews = "Rubrika Staò se novináøem:" + Environment.NewLine + Environment.NewLine + "Scott:" + Environment.NewLine + "A u dost! U mám po krk tìchhle pokøivenıch kreatur. Mì nezajímá, co si myslí" + Environment.NewLine + "vìdci. Tohle u je fakt moc! Vèera jsem vidìl jednoho chlápka rozmetat zeï" + Environment.NewLine + "budovy na padr. Normálnì bez problému si pøišel a bouchl do zdi. To byste" + Environment.NewLine + "nevìøili, jaká spouš po nìm zùstala. A navíc ta budova je teï odepsaná k demolici." + Environment.NewLine + "No co si o tom máme myslet? Já vám to øeknu! Kdy se nemùeme spolehnout" + Environment.NewLine + " na policii tak si musíme zøídit poøádek sami. Musíme se vydat do ulic a skoncovat" + Environment.NewLine + " to s nimi. Vyzıvám obyvatele Washingtonu, aby pozítøí 13. øíjna vyšli se mnou" + Environment.NewLine + " do ulic a jednou pro vdy ukázali, jak to tady chodí!" + Environment.NewLine + Environment.NewLine + "Komentáøe:" + Environment.NewLine + "Freebird246:Autor èlánku asi omylem zapomnìl zmínit, e ten dotyènı chlápek" + Environment.NewLine + " znièil èást hoøící budovy, aby zachránil její obyvatele!" + Environment.NewLine + "13:58";
+        #endregion
         private Texture2D menuBackground, menuItem_Exit, menuItem_Loadgame, menuItem_Options,menuItem_Newgame,
-            iconMouse, menuItem_Temporary, menuItem_Back, hero, classEnforcer, classMastermind, pozadiNG;
+            iconMouse, menuItem_Temporary, menuItem_Back, hero, classEnforcer, classMastermind, pozadiNG, iconTemp;
         private List<Texture2D> mainMenuTextury = new List<Texture2D>(); 
         private List<Texture2D> optMenuTextury = new List<Texture2D>();
         private List<Texture2D> newgameMenuTextury = new List<Texture2D>();
@@ -28,7 +32,8 @@ namespace Imprisoned_Hope
         private float menuSpeed = 1.5f;
         private int menuX = 760;
         private int menuY = 450;
-        public SpriteFont FontCourierNew;
+        private int ngmenuX = 15, ngmenuY = 140;
+        public SpriteFont FontCourierNew, FontTimes;
         public Song music_menuTheme;
         public int sirka = 1280;
         public int vyska = 720;
@@ -72,9 +77,10 @@ namespace Imprisoned_Hope
 
             iconMouse = Content.Load<Texture2D>(@"Textury\iconMouse");
 
-            music_menuTheme = Content.Load<Song>(@"Music\music_menuTheme");
+            //music_menuTheme = Content.Load<Song>(@"Music\music_menuTheme");
 
             FontCourierNew = Content.Load<SpriteFont>(@"Fonty\courier_new");
+            FontTimes = Content.Load<SpriteFont>(@"Fonty\times");
 
             #region Naèítání textur patøících do listù
 
@@ -85,18 +91,20 @@ namespace Imprisoned_Hope
 
             optMenuTextury.Add(menuItem_Temporary = Content.Load<Texture2D>(@"Textury\Menu\Temporary"));
             optMenuTextury.Add(menuItem_Back = Content.Load<Texture2D>(@"Textury\Menu\Back"));
-
-            newgameMenuTextury.Add(classEnforcer = Content.Load<Texture2D>(@"Textury\enforcer_class"));
+           
+            pozadiNG = Content.Load<Texture2D>(@"Textury\pozadiNG");
             newgameMenuTextury.Add(classMastermind = Content.Load<Texture2D>(@"Textury\mastermind_class"));
-            newgameMenuTextury.Add(pozadiNG = Content.Load<Texture2D>(@"Textury\pozadiNG"));
+            newgameMenuTextury.Add(classEnforcer = Content.Load<Texture2D>(@"Textury\enforcer_class"));
+            newgameMenuTextury.Add(iconTemp = Content.Load<Texture2D>(@"Textury\temp"));
+            newgameMenuTextury.Add(iconTemp = Content.Load<Texture2D>(@"Textury\temp"));
 
             #endregion   
          
             #region naplòování menu listama textur a urèování startovní pozice
 
             mainMenu = new Menu(mainMenuTextury, new Rectangle(menuX, menuY, 0, 0), menuSpeed, menuX, menuY);
-            optMenu = new Menu(optMenuTextury, new Rectangle(menuX, vyska + 1, 0, 0), menuSpeed, menuX, menuY); 
-            
+            optMenu = new Menu(optMenuTextury, new Rectangle(menuX, vyska + 1, 0, 0), menuSpeed, menuX, menuY);
+            newGameMenu = new Menu(pozadiNG, newgameMenuTextury, new Rectangle(ngmenuX, ngmenuY, pozadiNG.Width, pozadiNG.Height), menuSpeed, ngmenuX, ngmenuY, 45, 50, 100);           
             #endregion
 
             //MediaPlayer.Play(music_menuTheme);
@@ -165,9 +173,13 @@ namespace Imprisoned_Hope
 
             spriteBatch.Draw(hero, new Rectangle(0,50,hero.Width, hero.Height), Color.White);
             #region NewGame menu (Work in progress)
-            spriteBatch.Draw(pozadiNG, new Rectangle(15, 140, pozadiNG.Width, pozadiNG.Height), Color.White);
-            spriteBatch.Draw(classMastermind, new Rectangle(50, 190, classMastermind.Width, classMastermind.Height), Color.White);
-            spriteBatch.Draw(classEnforcer, new Rectangle(150, 190, classEnforcer.Width, classEnforcer.Height), Color.White);
+            /*spriteBatch.Draw(pozadiNG, new Rectangle(ngmenuX, ngmenuY, pozadiNG.Width, pozadiNG.Height), Color.White);
+            spriteBatch.Draw(classMastermind, new Rectangle(ngmenuX + 45, ngmenuY + 50, classMastermind.Width, classMastermind.Height), Color.White);
+            spriteBatch.Draw(classEnforcer, new Rectangle(ngmenuX + 145, ngmenuY + 50, classEnforcer.Width, classEnforcer.Height), Color.White);
+            spriteBatch.Draw(iconTemp, new Rectangle(ngmenuX + 245, ngmenuY + 50, iconTemp.Width, iconTemp.Height), Color.White);
+            spriteBatch.Draw(iconTemp, new Rectangle(ngmenuX + 345, ngmenuY + 50, iconTemp.Width, iconTemp.Height), Color.White);
+            spriteBatch.DrawString(FontTimes, mastermindNews, new Vector2((float)ngmenuX + 15,(float)ngmenuY + 125), Color.Black);
+            spriteBatch.DrawString(FontTimes, enforcerNews, new Vector2((float)ngmenuX + 15, (float)ngmenuY + 125), Color.Black);*/
             #endregion
 
             //foreach (MenuItem s in mainMenu.MenuItems)
@@ -181,8 +193,9 @@ namespace Imprisoned_Hope
             //nová metoda v Menu Draw
             mainMenu.DrawMenu(graphics, spriteBatch);
             optMenu.DrawMenu(graphics, spriteBatch);
+            newGameMenu.DrawMenu(graphics, spriteBatch);
 
-            spriteBatch.DrawString(FontCourierNew, "Verze alpha 0.0025" + mainMenu.MenuDirection, new Vector2(0, 0), Color.White);
+            spriteBatch.DrawString(FontCourierNew, "Verze alpha 0.0026" + mainMenu.MenuDirection, new Vector2(0, 0), Color.White);
             
             
             spriteBatch.Draw(iconMouse, new Rectangle(mys.X-15, mys.Y-10, iconMouse.Width, iconMouse.Height), Color.White); //Vykreslení myši (musí bıt poslední)
--- Hard_Try/Hard_Try/Menu.cs
+++ Hard_Try/Hard_Try/Menu.cs
@@ -101,8 +101,44 @@ namespace Imprisoned_Hope
 			this.Rectangle.Width = width;
 			this.Rectangle.Height = height;
 		}
+        /// <summary>
+        /// Pro menu NewGame menu orientované po ose X
+        /// </summary>
+        /// <param name="Texture">textura pozadí</param>
+        /// <param name="list">List s texturami</param>
+        /// <param name="rect">Rect pozadí (asi)</param>
+        /// <param name="speed">Rychlos posunu (float)</param>
+        /// <param name="dockX">Pozice X kde se menu zastaví</param>
+        /// <param name="dockY">Pozice Y kde se menu zastaví</param>
+        /// <param name="odstupX">Odsazení od textury</param>
+        /// <param name="odstupY">Odsazení od textury</param>
+        /// <param name="mezera">Mezera mezi ikonami</param>
+        public Menu(Texture2D Texture, List<Texture2D> list, Rectangle rect, float speed, int dockX, int dockY, int odstupX, int odstupY, int mezera)
+        {
+            this.Texture = Texture;
+            this.Rectangle = rect;
+            this.Position = new Vector2(rect.X, rect.Y);
+            this.Speed = speed;
+            this.DockX = dockX;
+            this.DockY = dockY;
+            int x = rect.X + odstupX;
+            int y = rect.Y;
+            int width = 0;
+            int height = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                MenuItems.Add(new MenuItem(list[i], new Rectangle(x, y + odstupY, list[i].Width, list[i].Height), Color.White));
+                x += list[i].Height + mezera;
+                width += list[i].Width + mezera;
+                if (list[i].Height > height)
+                {
+                    height = list[i].Height;
+                }
 
-
+            }
+            this.Rectangle.Width = width;
+            this.Rectangle.Height = height;
+        }
 		public void updateItemsPosition()//nastaví všem itemům pozici aby byli pod sebou a se stejným začátkem jako objekt menu
 		{
 			int x = this.Rectangle.X;
@@ -219,7 +255,7 @@ namespace Imprisoned_Hope
 			//vykreslení menu a jeho textury
 			if (Texture != null)
 			{
-				spriteBatch.Draw(Texture, Rectangle, Color);    
+				spriteBatch.Draw(Texture, Rectangle, Color.White);    
 			}
 			
 			//vykreslení položek
